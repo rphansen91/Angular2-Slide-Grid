f2c46db81adafd8c8e0b5eee6c02d66c18e8fd47
rphansen91 <ryan@antengo.com>
2015-11-17 11:33:11 -0800

    Scale

--- src/app/display/listingDisplay.js
+++ src/app/display/listingDisplay.js
@@ -50,7 +50,7 @@ var ListingDisplay = (function () {
         angular2_1.View({
             directives: [angular2_1.NgIf],
             styles: [
-                '.listingDisplay {position: relative; float: left; z-index: 1; background-color: rgba(174, 146, 204, 0.8); background-size: 100%; background-repeat: no-repeat;}',
+                '.listingDisplay {position: relative; float: left; z-index: 1; background-color: rgba(174, 146, 204, 0.8); background-size: 100%; background-repeat: no-repeat;-ms-transform: scale(1); -webkit-transform: scale(1); transform: scale(1);}',
                 '.opening {-ms-transform: scale(1.3); -webkit-transform: scale(1.3); transform: scale(1.3); z-index: 2;}',
                 '.price {position: absolute;bottom: 0px;right: 0px;color: rgb(255, 255, 255);font-size: 20px;text-shadow: black 2px 2px 3px;line-height: 35px;padding: 0px 18px;background-color: rgba(174, 146, 204, 0.8);font-family: Arial;}',
                 '.title {position: absolute;bottom: 0px;right: 0px; left: 0px; color: rgb(255, 255, 255);font-size: 20px;text-shadow: black 2px 2px 3px;line-height: 35px;padding: 0px 18px;background-color: rgba(174, 146, 204, 0.8);font-family: Arial; }'
